c023648d92487cf7aa020e69b71ee98e67c8b2b3
ArseneLuko <arsene.luko@gmail.com>
2026-02-05 15:10:22 +0100

    add: dicemath mine - initial commit

---
+++ dicemath.py
@@ -0,0 +1,26 @@
+class Game:
+    def __init__(self) -> None:
+        pass
+
+
+    def run(self):
+        # set-up the game (width, height, dice number) or default values
+        # lets play_game():
+        # afte end show results
+        # do you want to play again? - all this in while Loop
+        pass
+
+
+    def play_game(self):
+        # if enough time play:
+        # create diceses
+        # check if they don't overlap
+        # print them on screen
+        # let the player answer
+        # check for answer and manage points
+        pass
+
+
+if __name__ == '__main__':
+    new_game = Game()
+    new_game.run()
